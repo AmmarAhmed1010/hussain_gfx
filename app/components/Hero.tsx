ccbfe6a329a8224371dbac547103cc0ef4560194
Ammar Ahmed <ammar7298@gmail.com>
2025-02-21 13:32:43 +0500

    slider posiiton change

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -10,12 +10,13 @@ const Hero: React.FC = () => {
       <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to Hussain GFX</h1>
       <h2 className="text-2xl md:text-4xl font-semibold mb-4 text-[#E900B9]">Creative & Professional Designs</h2>
       <h3 className="text-xl md:text-3xl mb-6">Thumbnails That Stand Out</h3>
+      <Slider/>
       <Link href="/my-work">
         <button className="bg-[#E900B9] text-white px-6 py-3 rounded-3xl text-lg font-semibold flex items-center gap-2 hover:bg-[#d100a6] transition duration-300">
           See Latest Work <FaArrowRight />
         </button>
       </Link>
-      <Slider/>
+   
     </div>
   );
 };
