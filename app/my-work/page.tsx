7956e4f169ed1d90e57a9c67369aa29289fa69dc
Ammar Ahmed <ammar7298@gmail.com>
2025-02-24 13:42:44 +0500

    new images addded

--- app/my-work/page.tsx
+++ app/my-work/page.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import Image from 'next/image';
 
 const page = () => {
-  const images = Array.from({ length: 8 }, (_, i) => `/slider/slider (${i + 1}).jpg`);
+  const sliderImages = Array.from({ length: 8 }, (_, i) => `/slider/slider (${i + 1}).jpg`);
+  const myWorkImages = Array.from({ length: 5 }, (_, i) => `/mywork/mywork (${i + 1}).jpg`);
+
+  const images = [...sliderImages, ...myWorkImages];
 
   return (
     <div className="grid grid-cols-1 mt-20 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
       {images.map((src, index) => (
-        <div key={index} className="border border-white rounded-xl overflow-hidden">
-          <Image src={src} alt={`slider ${index + 1}`} width={300} height={300} className="w-full h-auto" />
+        <div key={index} className="border border-white rounded-xl overflow-hidden w-full h-auto md:h-[300px] flex items-center justify-center">
+          <Image
+            src={src}
+            alt={`image ${index + 1}`}
+            width={300}
+            height={300}
+            className="w-full h-auto md:h-full md:object-cover"
+          />
         </div>
       ))}
     </div>
