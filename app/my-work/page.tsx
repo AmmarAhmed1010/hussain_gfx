e78cfa3b79e730afd8a614bc28a1f6821140ddbd
Ammar Ahmed <ammar7298@gmail.com>
2025-02-24 11:30:42 +0500

    mt-20

--- app/my-work/page.tsx
+++ app/my-work/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   const images = Array.from({ length: 8 }, (_, i) => `/slider/slider (${i + 1}).jpg`);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+    <div className="grid grid-cols-1 mt-20 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
       {images.map((src, index) => (
         <div key={index} className="border border-white rounded-xl overflow-hidden">
           <Image src={src} alt={`slider ${index + 1}`} width={300} height={300} className="w-full h-auto" />
